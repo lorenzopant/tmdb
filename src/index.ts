f8db45d65393b04f822aa8bd3cf8eaf1240ec585
Lorenzo Pantano <l.pantano97@gmail.com>
2025-06-29 00:26:55 +0200

    chore: refactor export all types

--- src/index.ts
+++ src/index.ts
@@ -3,41 +3,4 @@
 export { TMDB } from "./tmdb";
 export { TMDBError } from "./errors/tmdb";
 
-export type {
-	MovieDetails,
-	MovieAlternativeTitle,
-	MovieAlternativeTitles,
-	MovieCredits,
-	MovieExternalIDs,
-	MovieImages,
-	MovieKeywords,
-	MovieReleaseDates,
-	MovieTranslations,
-	MovieVideos,
-} from "./types/movies";
-export type {
-	Changes,
-	Collection,
-	Genre,
-	ImageItem,
-	Keyword,
-	ProductionCompany,
-	ProductionCountry,
-	SpokenLanguage,
-	VideoItem,
-	Cast,
-	Crew,
-} from "./types/common";
-export type {
-	SearchMoviesParams,
-	MovieDetailsParams,
-	MovieListParams,
-	PaginatedResponse,
-	MovieAlternativeTitlesParams,
-} from "./types/params";
-
-// ** Internationalization
-export type { CountryISO3166_1 } from "./types/countries";
-export { TMDBCountries } from "./types/countries";
-export type { LanguageISO6391 } from "./types/lang";
-export { Languages } from "./types/lang";
+export * from "./types";
---
+++ src/types/index.ts
@@ -0,0 +1,7 @@
+export * from "./common";
+export * from "./countries";
+export * from "./enums";
+export * from "./lang";
+export * from "./movies";
+export * from "./params";
+export * from "./utility";
