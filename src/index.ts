dbf325458d966e1b95aefef14c34a691ade1ebd3
Lorenzo Pantano <l.pantano97@gmail.com>
2025-06-29 10:50:49 +0200

    update: removed react hook

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,4 @@
 export { TMDB } from "./tmdb";
 export { TMDBError } from "./errors/tmdb";
 
-export { TMDBProvider } from "./provider/tmdb-provider";
-export { useTmdb } from "./provider/tmdb-provider";
-
 export * from "./types";
