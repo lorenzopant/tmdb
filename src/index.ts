4afcc8d1a5c93a8483ce5b016e70cbc1324b15e2
Lorenzo Pantano <l.pantano97@gmail.com>
2025-06-29 10:40:38 +0200

    feat: added provider and react hook #6

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 // src/index.ts
-
 export { TMDB } from "./tmdb";
 export { TMDBError } from "./errors/tmdb";
+
+export { TMDBProvider } from "./provider/tmdb-provider";
+export { useTmdb } from "./provider/tmdb-provider";
 
 export * from "./types";
