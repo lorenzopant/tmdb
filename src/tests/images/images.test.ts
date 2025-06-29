9f3fa3cc3a670bcf999dc3dde64839480ea4ba21
Lorenzo Pantano <l.pantano97@gmail.com>
2025-06-29 23:22:01 +0200

    test: added image tests

---
+++ src/tests/images/images.test.ts
@@ -0,0 +1,45 @@
+import { describe, expect, test } from "vitest";
+import { IMAGE_SECURE_BASE_URL, IMAGE_BASE_URL } from "../../types/images";
+import { ImageAPI } from "../../images/images";
+
+describe("ImageAPI", () => {
+	const path = "/sample.jpg";
+
+	test("should generate secure poster URL by default", () => {
+		const imageAPI = new ImageAPI();
+		const poster_path = "/1E5baAaEse26fej7uHcjOgEE2t2.jpg"; // Fast X
+		const url = imageAPI.poster(poster_path);
+		expect(url).toBe(`${IMAGE_SECURE_BASE_URL}w500${poster_path}`);
+	});
+
+	test("should generate non-secure poster URL when configured", () => {
+		const poster_path = "/1E5baAaEse26fej7uHcjOgEE2t2.jpg"; // Fast X
+		const imageAPI = new ImageAPI(false);
+		const url = imageAPI.poster(poster_path);
+		expect(url).toBe(`${IMAGE_BASE_URL}w500${poster_path}`);
+	});
+
+	test("should generate backdrop URL", () => {
+		const imageAPI = new ImageAPI();
+		const url = imageAPI.backdrop(path, "w1280");
+		expect(url).toBe(`${IMAGE_SECURE_BASE_URL}w1280${path}`);
+	});
+
+	test("should generate logo URL", () => {
+		const imageAPI = new ImageAPI();
+		const url = imageAPI.logo(path, "w300");
+		expect(url).toBe(`${IMAGE_SECURE_BASE_URL}w300${path}`);
+	});
+
+	test("should generate profile URL", () => {
+		const imageAPI = new ImageAPI();
+		const url = imageAPI.profile(path, "h632");
+		expect(url).toBe(`${IMAGE_SECURE_BASE_URL}h632${path}`);
+	});
+
+	test("should generate still URL", () => {
+		const imageAPI = new ImageAPI();
+		const url = imageAPI.still(path, "w300");
+		expect(url).toBe(`${IMAGE_SECURE_BASE_URL}w300${path}`);
+	});
+});
