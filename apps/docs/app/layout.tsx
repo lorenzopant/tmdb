53edd35f992a1c6843b76542b795ccf1e8ca435d
Lorenzo Pantano <l.pantano97@gmail.com>
2026-02-26 19:45:36 +0100

    chore: favicon inline

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -19,6 +19,12 @@ const inter = Inter({
 export default function Layout({ children }: LayoutProps<"/">) {
 	return (
 		<html lang="en" className={inter.className} suppressHydrationWarning>
+			<head>
+				<link
+					rel="icon"
+					href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🍿</text></svg>"
+				/>
+			</head>
 			<body className="flex flex-col min-h-screen">
 				<RootProvider>{children}</RootProvider>
 				<Analytics />
