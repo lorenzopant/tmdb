9ea04fdb415920c27a3c70713a4df925bd417412
Lorenzo Pantano <l.pantano97@gmail.com>
2026-02-26 19:19:00 +0100

    feat: added title and favicon

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -1,19 +1,28 @@
-import { RootProvider } from 'fumadocs-ui/provider/next';
-import './global.css';
-import { Inter } from 'next/font/google';
-import { Analytics } from '@vercel/analytics/next';
+import { RootProvider } from "fumadocs-ui/provider/next";
+import "./global.css";
+import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
+import type { Metadata } from "next";
+
+const emojiSvg =
+	"image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>🍿</text></svg>";
+
+export const meta: Metadata = {
+	title: "lorenzopant/tmdb",
+	icons: emojiSvg,
+};
 
 const inter = Inter({
-  subsets: ['latin'],
+	subsets: ["latin"],
 });
 
-export default function Layout({ children }: LayoutProps<'/'>) {
-  return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
-      <body className="flex flex-col min-h-screen">
-        <RootProvider>{children}</RootProvider>
-        <Analytics />
-      </body>
-    </html>
-  );
+export default function Layout({ children }: LayoutProps<"/">) {
+	return (
+		<html lang="en" className={inter.className} suppressHydrationWarning>
+			<body className="flex flex-col min-h-screen">
+				<RootProvider>{children}</RootProvider>
+				<Analytics />
+			</body>
+		</html>
+	);
 }
