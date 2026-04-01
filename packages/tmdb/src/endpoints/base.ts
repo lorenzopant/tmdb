fcf98cbc7f7ec229f1afaee597da2ead0de69929
Lorenzo Pantano <l.pantano97@gmail.com>
2026-04-01 10:25:18 +0200

    fix: actually export standalone classes

--- packages/tmdb/src/endpoints/base.ts
+++ packages/tmdb/src/endpoints/base.ts
@@ -1,13 +1,23 @@
 import { ApiClient } from "../client";
 import { Language } from "../types";
 import { TMDBOptions } from "../types/config";
+import { Errors } from "../errors/messages";
 
 export abstract class TMDBAPIBase {
 	protected client: ApiClient;
 	protected defaultOptions: TMDBOptions;
 
-	constructor(client: ApiClient, defaultOptions: TMDBOptions = {}) {
-		this.client = client;
+	constructor(accessTokenOrClient: string | ApiClient, defaultOptions: TMDBOptions = {}) {
+		if (typeof accessTokenOrClient === "string") {
+			if (!accessTokenOrClient) throw new Error(Errors.NO_ACCESS_TOKEN);
+			this.client = new ApiClient(accessTokenOrClient, {
+				logger: defaultOptions.logger,
+				deduplication: defaultOptions.deduplication,
+				interceptors: defaultOptions.interceptors,
+			});
+		} else {
+			this.client = accessTokenOrClient;
+		}
 		this.defaultOptions = defaultOptions;
 	}
 
@@ -18,7 +28,11 @@ export abstract class TMDBAPIBase {
 	 */
 	protected applyDefaults<T extends object>(params?: T): T | undefined {
 		const { language, region } = this.defaultOptions;
-		return { ...(language !== undefined && { language }), ...(region !== undefined && { region }), ...params } as T;
+		return {
+			...(language !== undefined && { language }),
+			...(region !== undefined && { region }),
+			...params,
+		} as T;
 	}
 
 	/**
